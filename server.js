51adf31e8f8a053a94cc179da05ca50d93f5dcb5
Edward Deam <edwardcrdeam@gmail.com>
2019-07-09 09:49:43 +1000

    Added endpoint for testing

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const mongoose = require("mongoose");
 const PORT = 5000;
 const mongoURI = "mongodb://localhost/gitanswer";
 
-mongoose.connect(mongoURI, { useNewUrlParser: true }, err => {
-  if (err) return console.log(`${err}`);
-  console.log("connected to mongodb");
-});
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => {
+    console.info("Connected to MongoDB");
+  })
+  .catch(error => {
+    throw new Error(`Unable to connect to database: ${mongoURI}`);
+  });
+
+app.get('/', (req,res)=> {
+  res.send("Accessed Endpoint!!");
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
