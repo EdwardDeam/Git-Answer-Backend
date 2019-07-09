18c3480419111d130ac2198eef092d82dc79fd84
Owen <owshepherd@gmail.com>
2019-07-09 14:56:12 +1000

    Created Post schema

---
+++ models/Post.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    user_id, // difficulty reading
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  page_views: {
+    type: Number
+  },
+  tags: {},
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  comments: {},
+  votes: {}
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
