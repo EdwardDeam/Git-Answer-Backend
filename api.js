131b9a121d4c8039b17683f55a10e25dcf68395d
Tony Xu <Design.TXU@gmail.com>
2019-07-09 14:38:47 +1000

    Fixed Backend Deployment

--- api.js
+++ api.js
@@ -2,14 +2,8 @@ const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = 5000;
-const mongoLOCAL = "mongodb://localhost/gitanswer";
-
-const result = dotenv.config()
-if (result.error) {
-  throw result.error
-}
-console.log(result.parsed)
+const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 mongoose
   .connect(process.env.DB_URL, { useNewUrlParser: true, useCreateIndex: true })
@@ -20,9 +14,9 @@ mongoose
     throw new Error(`Unable to connect to database: ${process.env.DB_URL}`);
   });
 
-app.get('/test', (req,res)=> {
+app.get("/test", (req, res) => {
   res.status(200).send("Accessed Endpoint!!");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
