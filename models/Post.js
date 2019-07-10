1fc143b6e3525a1640f3a43623e8f79284efdcb6
Edward Deam <edwardcrdeam@gmail.com>
2019-07-10 12:32:07 +1000

    Added validation to posts

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema({
   title: String,
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   date: { type: Date, default: Date.now },
@@ -11,4 +11,26 @@ const PostSchema = new mongoose.Schema({
   votes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Vote" }]
 });
 
-module.exports = Post = mongoose.model("post", PostSchema);
+const Post = mongoose.model("post", PostSchema);
+
+const validatePost = post => {
+  const schema = Joi.object().keys({
+    title: Joi.string()
+      .min(15)
+      .max(150)
+      .required(),
+    author: Joi.string()
+      .regex(/^[0-9a-fA-F]{24}$/, "mongo object id")
+      .required(),
+    text: Joi.string()
+      .min(200)
+      .required(),
+    pageViews: Joi.number()
+      .integer()
+      .positive()
+  });
+};
+module.exports = {
+  Post,
+  postSchema
+};
