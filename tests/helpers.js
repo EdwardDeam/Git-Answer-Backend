4eb1314ef38336b95f3196766654ad2f985fe281
Edward Deam <edwardcrdeam@gmail.com>
2019-07-11 14:41:54 +1000

    Removed duplicate tests

--- tests/helpers.js
+++
@@ -1,7 +0,0 @@
-const supertest = require("supertest");
-const chai = require("chai");
-const app = require("../express");
-
-global.app = app;
-global.expect = chai.expect;
-global.request = supertest(app);
